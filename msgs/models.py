2d1e664dd531f3b24bc2a9c158560d9dfb785025
Faride Tari <faridetari@gmail.com>
2024-07-05 12:19:31 -0700

    third

--- msgs/models.py
+++ msgs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.core.validators import RegexValidator, MinLengthValidator
 # Create your models here.
+class Msg(models.Model):
+    name = models.CharField(max_length=15, validators=[
+        RegexValidator('^[A-Za-z0-9_]+$'),
+        MinLengthValidator(3)
+    ])
+    msgText = models.CharField(max_length=30, validators=[
+        MinLengthValidator(2)
+    ])
+
+    class Meta:
+        ordering = ['-id']
